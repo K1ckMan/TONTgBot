49826808eae68ac0f3d3e1e4044060efdcaa67c0
Konstantins Gulaks <k.gulaks@dynatech.lv>
2024-07-29 16:24:53 +0300

    fix

--- bot.py
+++ bot.py
@@ -2510,12 +2510,13 @@ def command_timediff(message):
     bot.send_message(config.tg, text=_("Can't get top processes"), reply_markup=markuplinux)
 # /Top processes
 
-bot.message_handler(func=lambda message: message.text == lt_containers)
+@bot.message_handler(func=lambda message: message.text == lt_containers)
 def command_containers(message):
     try:
         bot.send_chat_action(config_tg, "typing")
-        bot.send_chat_action(config.tg, "typing")
-        bot.send_chat_action(config.tg, "typing")
+        containers_info = "docker ps"
+        containers_info = str(subprocess.check_output(containers_info, shell=True, encoding='utf-8'))
+        bot.send_message(config_tg, text=f"```\n{containers_info}\n```", parse_mode='Markdown')
     except Exception as e:
         bot.send_message(config_tg, text=f"Can't get container info: {e}")
 
